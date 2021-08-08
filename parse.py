dd52a1cddc7b1dd99a9bab0d67e7a4183e583f63
Pricolno <sashadaanton@mail.ru>
2021-08-09 00:17:27 +0300

    add parse brovse-page (get information about name, cost and city)

---
+++ parse.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ria.com/newauto/marka-opel/'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.135 YaBrowser/21.6.3.756 Yowser/2.5 Safari/537.36',
+    'accept': '*/*'
+}
+HOST = 'https://auto.ria.com'
+
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='mhide')
+    # print(pagination)
+    if pagination:
+        # print(int(pagination[-1].get_text()))
+        return int(pagination[-1].get_text())
+    else:
+        return 1
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('section', class_='proposition')
+    cars = []
+    for item in items:
+        cars.append({
+            'title': item.find('span', class_='link').get_text(strip=True),
+            'link': HOST + item.find('a', class_='proposition_link').get('href'),
+            'usd_price': item.find('span', class_='green').get_text(strip=True),
+            'city': item.find('span', class_='item').get_text(strip=True)
+        })
+    for car in cars:
+        print(car)
+    print(len(cars))
+    return cars
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        cars = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1, pages_count + 1):
+            print(f'Парсинг страницы {page} из {pages_count}...')
+            html = get_html(URL, params={'page': page})
+            cars.extend(get_content(html.text))
+        print(f'Получено {len(cars)} автомобилей')
+
+    else:
+        print('Error')
+
+
+parse()
